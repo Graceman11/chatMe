145bed283077b169bfd8e3782839e5ab348cf2fb
Graceman11 <stevenson3310@gmail.com>
2023-06-01 13:43:53 +0100

    init commit

--- server/controllers/DeleteMessageController.js
+++ server/controllers/DeleteMessageController.js
@@ -1,7 +1,9 @@
 const messageSchema = require("../models/messageSchema");
+const { findByIdAndRemove } = require("../models/post");
 
 const deleteMessage = async (req, res, next) => {
   try {
+    const delMessage = await findByIdAndRemove
   } catch {}
 };
 
